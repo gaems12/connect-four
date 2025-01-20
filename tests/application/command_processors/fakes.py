d34531236b5af4e79ac8d09755bfae68584b117c
Madnoberson <baseddepartmentzx77@gmail.com>
2025-01-20 18:39:04 +0400

    Add `test_create_game_processor`

---
+++ tests/application/command_processors/fakes.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2024, Egor Romanov.
+# All rights reserved.
+
+from four_in_a_row.domain import GameId, UserId, Game
+from four_in_a_row.application import (
+    SortGamesBy,
+    GameGateway,
+    Event,
+    EventPublisher,
+)
+
+
+class FakeGameGateway(GameGateway):
+    __slots__ = ("_games",)
+
+    def __init__(self, games: dict[GameId, Game]):
+        self._games = games
+
+    @property
+    def games(self) -> list[Game]:
+        return list(self._games.values())
+
+    async def by_id(
+        self,
+        id: GameId,
+        *,
+        acquire: bool = False,
+    ) -> Game | None:
+        return self._games.get(id)
+
+    async def list_by_player_ids(
+        self,
+        player_ids: tuple[UserId, UserId],
+        *,
+        sort_by: SortGamesBy | None = None,
+        limit: int = 0,
+    ) -> list[Game]:
+        games = [
+            game
+            for game in self._games.values()
+            if player_ids[0] in game.players and player_ids[1] in game.players
+        ]
+        if sort_by == SortGamesBy.DESC_CREATED_AT:
+            games.sort(key=lambda g: g.created_at, reverse=True)
+
+        if limit > 0:
+            games = games[:limit]
+        else:
+            raise Exception(
+                "FakeGameGateway. Cannot list_by_player_ids: "
+                "limit is not a positive number or zero.",
+            )
+
+        return games
+
+    async def save(self, game: Game) -> None:
+        self._games[game.id] = game
+
+    async def update(self, game: Game) -> None:
+        self._games[game.id] = game
+
+
+class FakeEventPublisher(EventPublisher):
+    __slots__ = ("_events",)
+
+    def __init__(self, events: list[Event]):
+        self._events = events
+
+    @property
+    def events(self) -> list[Event]:
+        return self._events
+
+    async def publish(self, event: Event) -> None:
+        self._events.append(event)
---
+++ tests/application/command_processors/test_create_game.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2024, Egor Romanov.
+# All rights reserved.
+
+from unittest.mock import AsyncMock
+from datetime import datetime, timedelta, timezone
+from typing import Final
+
+from uuid_extensions import uuid7
+
+from four_in_a_row.domain import (
+    ChipType,
+    BOARD_COLUMNS,
+    BOARD_ROWS,
+    GameId,
+    UserId,
+    PlayerState,
+    CreateGame,
+)
+from four_in_a_row.application import (
+    GameCreatedEvent,
+    CreateGameCommand,
+    CreateGameProcessor,
+)
+from .fakes import FakeGameGateway, FakeEventPublisher
+
+
+_GAME_ID: Final = GameId(uuid7())
+
+_FIRST_PLAYER_ID: Final = UserId(uuid7())
+_SECOND_PLAYER_ID: Final = UserId(uuid7())
+
+_TIME_FOR_EACH_PLAYER: Final = timedelta(minutes=1)
+
+
+async def test_create_game_processor():
+    event_publisher = FakeEventPublisher([])
+
+    command = CreateGameCommand(
+        game_id=_GAME_ID,
+        first_player_id=_FIRST_PLAYER_ID,
+        second_player_id=_SECOND_PLAYER_ID,
+        time_for_each_player=_TIME_FOR_EACH_PLAYER,
+        created_at=datetime.now(timezone.utc),
+    )
+    command_processor = CreateGameProcessor(
+        create_game=CreateGame(),
+        game_gateway=FakeGameGateway({}),
+        event_publisher=event_publisher,
+        transaction_manager=AsyncMock(),
+    )
+
+    await command_processor.process(command)
+
+    players = {
+        _FIRST_PLAYER_ID: PlayerState(
+            chip_type=ChipType.FIRST,
+            time_left=_TIME_FOR_EACH_PLAYER,
+        ),
+        _SECOND_PLAYER_ID: PlayerState(
+            chip_type=ChipType.SECOND,
+            time_left=_TIME_FOR_EACH_PLAYER,
+        ),
+    }
+    expected_event = GameCreatedEvent(
+        game_id=command.game_id,
+        board=[[None] * BOARD_COLUMNS for _ in range(BOARD_ROWS)],
+        players=players,
+        current_turn=_FIRST_PLAYER_ID,
+    )
+    assert expected_event in event_publisher.events
