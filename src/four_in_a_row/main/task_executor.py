e53b4094ba5f58cf97eb2b66564a81117611526b
Madnoberson <baseddepartmentzx77@gmail.com>
2025-01-25 11:24:37 +0400

    Fixes, refactor

--- src/four_in_a_row/main/task_executor.py
+++ src/four_in_a_row/main/task_executor.py
@@ -6,22 +6,22 @@ from taskiq_nats import PushBasedJetStreamBroker
 from dishka.integrations.taskiq import setup_dishka
 
 from four_in_a_row.infrastructure import (
-    nats_config_from_env,
-    redis_config_from_env,
+    load_nats_config,
+    load_redis_config,
     taskiq_redis_schedule_source_factory,
     ioc_container_factory,
 )
 
 
 def create_task_executor_app() -> TaskiqScheduler:
-    nats_config = nats_config_from_env()
-    redis_config = redis_config_from_env()
+    nats_config = load_nats_config()
+    redis_config = load_redis_config()
 
     broker = PushBasedJetStreamBroker([nats_config.url])
-    schedule_source = taskiq_redis_schedule_source_factory(redis_config)
-
-    app = TaskiqScheduler(broker, [schedule_source])
     ioc_container = ioc_container_factory([])
-    setup_dishka(ioc_container, app)
+    setup_dishka(ioc_container, broker)
+
+    schedule_source = taskiq_redis_schedule_source_factory(redis_config)
+    app = TaskiqScheduler(broker, [schedule_source])
 
     return app
