1092ae16509d1d7c799e00641e25010c81214037
Madnoberson <baseddepartmentzx77@gmail.com>
2025-01-31 21:20:20 +0400

    Refactor web api

--- src/four_in_a_row/presentation/web_api/routes.py
+++ src/four_in_a_row/presentation/web_api/routes.py
@@ -7,10 +7,10 @@ from dishka.integrations.fastapi import FromDishka, inject
 from four_in_a_row.application import MakeMoveCommand, MakeMoveProcessor
 
 
-router = APIRouter(prefix="/api/v1/internal")
+router = APIRouter(prefix="/api/v1/webhooks/centrifugo")
 
 
-@router.post("/games/{game_id}/moves", tags=["centrifugo"])
+@router.post("/make-move")
 @inject
 async def make_move(
     *,
