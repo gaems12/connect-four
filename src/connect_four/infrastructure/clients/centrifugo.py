6969921b15cd7f33344aab320b65e0daaa0f9801
Madnoberson <baseddepartmentzx77@gmail.com>
2025-02-15 09:25:55 +0400

    Add retries to `HTTPXCentrifugoClient`

--- src/connect_four/infrastructure/clients/centrifugo.py
+++ src/connect_four/infrastructure/clients/centrifugo.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2024, Egor Romanov.
 # All rights reserved.
 
+import asyncio
+import random
+import traceback
 from urllib.parse import urljoin
 from dataclasses import dataclass
+from typing import Final
 
 from httpx import AsyncClient
 
@@ -16,6 +20,7 @@ from connect_four.application import (
     MoveRejectedEvent,
     Event,
 )
+from connect_four.infrastructure.log import RequestLogger
 from connect_four.infrastructure.utils import get_env_var
 
 
@@ -28,6 +33,13 @@ type _Serializable = (
     | list[_Serializable]
     | dict[str, _Serializable]
 )
+
+_MAX_RETRIES: Final = 20
+_BASE_BACKOFF_DELAY: Final = 0.5
+_MAX_BACKOFF_DELAY: Final = 10
+
+
+class CentrifuoClientError(Exception): ...
 
 
 def load_centrifugo_config() -> "CentrifugoConfig":
@@ -44,15 +56,17 @@ class CentrifugoConfig:
 
 
 class HTTPXCentrifugoClient:
-    __slots__ = ("_httpx_client", "_config")
+    __slots__ = ("_httpx_client", "_config", "_logger")
 
     def __init__(
         self,
         httpx_client: AsyncClient,
         config: CentrifugoConfig,
+        logger: RequestLogger,
     ):
         self._httpx_client = httpx_client
         self._config = config
+        self._logger = logger
 
     async def publish_event(self, event: Event) -> None:
         if isinstance(event, GameCreatedEvent):
@@ -198,19 +212,104 @@ class HTTPXCentrifugoClient:
         data: _Serializable,
     ) -> None:
         await self._send_request(
-            method="publish",
-            payload={"channel": channel, "data": data},
+            url=urljoin(self._config.url, "publish"),
+            json_={"channel": channel, "data": data},
+            retry_on_failure=True,
         )
 
     async def _send_request(
         self,
         *,
-        method: str,
-        payload: _Serializable,
+        url: str,
+        json_: _Serializable,
+        retry_on_failure: bool,
     ) -> None:
-        response = await self._httpx_client.post(
-            url=urljoin(self._config.url, method),
-            json=payload,
-            headers={"X-API-Key": self._config.api_key},
+        try:
+            self._logger.debug(
+                message="Going to make request to centrifugo.",
+                url=url,
+                json=json_,
+            )
+            response = await self._httpx_client.post(
+                url=url,
+                json=json_,
+                headers={"X-API-Key": self._config.api_key},
+            )
+        except Exception as e:
+            error_message = (
+                "Unexpected error occurred during request to centrifugo."
+            )
+            self._logger.error(
+                message=error_message,
+                traceback=traceback.format_exception(e),
+            )
+
+            if retry_on_failure:
+                retries_were_successful = await self._retry_request(
+                    url=url,
+                    json_=json_,
+                )
+                if retries_were_successful:
+                    return
+
+            raise CentrifuoClientError(error_message)
+
+        if response.status_code == 200:
+            self._logger.debug(
+                message="Centrifuo responded.",
+                status_code=response.status_code,
+                content=response.content.decode(),
+            )
+            return
+
+        error_message = "Centrifugo responded with bad status code."
+        self._logger.error(
+            message=error_message,
+            status_code=response.status_code,
+            content=response.content.decode(),
         )
-        response.raise_for_status()
+
+        if retry_on_failure:
+            retries_were_successful = await self._retry_request(
+                url=url,
+                json_=json_,
+            )
+            if retries_were_successful:
+                return
+
+        raise CentrifuoClientError(error_message)
+
+    async def _retry_request(
+        self,
+        *,
+        url: str,
+        json_: _Serializable,
+    ) -> bool:
+        for retry_number in range(1, _MAX_RETRIES + 1):
+            try:
+                self._logger.debug(
+                    message="Going to retry request to centrifugo.",
+                    retry_number=retry_number,
+                    retries_left=_MAX_RETRIES - retry_number,
+                )
+                await self._send_request(
+                    url=url,
+                    json_=json_,
+                    retry_on_failure=False,
+                )
+                return True
+
+            except CentrifuoClientError:
+                if retry_number == _MAX_RETRIES:
+                    return False
+
+                wait_time = self._calculate_backoff_wait_time(retry_number)
+                await asyncio.sleep(wait_time)
+
+        return False
+
+    def _calculate_backoff_wait_time(self, retry_number: int) -> float:
+        return min(
+            _BASE_BACKOFF_DELAY * (2**retry_number) + random.uniform(0, 0.5),
+            _MAX_BACKOFF_DELAY,
+        )
