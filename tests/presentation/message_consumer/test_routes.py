ff271b12a54ef3832b2d8c1c5e2443de472dc1f9
Madnoberson <baseddepartmentzx77@gmail.com>
2025-02-25 23:08:57 +0400

    Add new tests for message consumer

--- tests/presentation/message_consumer/test_routes.py
+++ tests/presentation/message_consumer/test_routes.py
@@ -12,11 +12,17 @@ from dishka import Provider, Scope, AsyncContainer, make_async_container
 from dishka.integrations.faststream import FastStreamProvider
 from uuid_extensions import uuid7
 
-from connect_four.application import CreateGameProcessor
+from connect_four.application import (
+    CreateGameProcessor,
+    EndGameProcessor,
+    MakeMoveProcessor,
+)
 from connect_four.infrastructure import get_env_var, common_retort_factory
 from connect_four.presentation.message_consumer import (
     create_broker,
     create_game_command_factory,
+    end_game_command_factory,
+    make_move_command_factory,
     ContextVarSetter,
     operation_id_factory,
 )
@@ -38,11 +44,23 @@ def ioc_container() -> AsyncContainer:
     provider.provide(ContextVarSetter, scope=Scope.REQUEST)
 
     provider.provide(create_game_command_factory, scope=Scope.REQUEST)
+    provider.provide(end_game_command_factory, scope=Scope.REQUEST)
+    provider.provide(make_move_command_factory, scope=Scope.REQUEST)
 
     provider.provide(
         lambda: AsyncMock(),
         scope=Scope.REQUEST,
         provides=CreateGameProcessor,
+    )
+    provider.provide(
+        lambda: AsyncMock(),
+        scope=Scope.REQUEST,
+        provides=EndGameProcessor,
+    )
+    provider.provide(
+        lambda: AsyncMock(),
+        scope=Scope.REQUEST,
+        provides=MakeMoveProcessor,
     )
 
     return make_async_container(provider, FastStreamProvider())
@@ -74,4 +92,36 @@ async def test_create_game(app: FastStream, broker: NatsBroker):
             message=message,
             subject="game.created",
             stream="connection_hub",
+        )
+
+
+async def test_end_game(app: FastStream, broker: NatsBroker):
+    async with (
+        TestApp(app),
+        TestNatsBroker(broker=broker, with_real=True) as test_broker,
+    ):
+        message = {"game_id": uuid7().hex}
+        await test_broker.publish(
+            message=message,
+            subject="game.ended",
+            stream="connection_hub",
+        )
+
+
+async def test_make_move(app: FastStream, broker: NatsBroker):
+    async with (
+        TestApp(app),
+        TestNatsBroker(broker=broker, with_real=True) as test_broker,
+    ):
+        message = {
+            "game_id": uuid7().hex,
+            "move": {
+                "column": 0,
+                "row": 0,
+            },
+        }
+        await test_broker.publish(
+            message=message,
+            subject="game.move_was_made",
+            stream="api_gateway",
         )
